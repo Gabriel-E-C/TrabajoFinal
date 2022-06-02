1dff2038862a4c9e166f15b3e0936c3ee5eb9a8e
Gaby-E-C <cavallone.gabriele@gmail.com>
2022-06-01 23:53:52 -0300

    Segundo Desafio React Agregar CartWidget

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import CartWidget from './components/CartWidget';
 import NavBar from './components/NavBar';
 
 function App() {
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,6 +12,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import CartWidget from './CartWidget';
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -126,33 +127,7 @@ const NavBar = () => {
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="../imagenes/logo-toys-r-u.png" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+            <CartWidget items={2}/>
           </Box>
         </Toolbar>
       </Container>
