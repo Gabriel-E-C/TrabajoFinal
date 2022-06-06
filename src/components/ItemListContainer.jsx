5bd82d0578bb6e779675cb00ad01239c13ab2bc0
Gaby-E-C <cavallone.gabriele@gmail.com>
2022-06-05 21:03:55 -0300

    Segundo Desafio React correccion faltaba ItemListContainer

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ItemListContainer (){
+    return (
+        <><h1>Aqui se encuentra la lista con los items que se comercializaran</h1></>
+        );
+};
+
+export default ItemListContainer;
